ce448fef18704f7629dff55b92af2868a234e7e4
Kerileng Soke <kerilengsoke4@gmail.com>
2024-07-22 23:45:08 +0200

    added for the app js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,1 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import PasswordReset from "./components/PasswordReset";
+import "./App.css";
 
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/password-reset" component={PasswordReset} />
+          {/* Add other routes as needed */}
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
