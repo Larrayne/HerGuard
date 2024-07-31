a879b52e6273af0dea1dc2aa991f95e968054718
tncube023a <tncube023a@student.wethinkcode.co.za>
2024-07-31 13:07:06 +0200

    trying to fix

--- Frontend/src/Login.js
+++ Frontend/src/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './App.css'; // Make sure to import your CSS file
+import './App.css'; 
 
 function Login() {
     return (
@@ -20,7 +20,7 @@ function Login() {
                     <Link to="/signup" className='custom-button w-100 rounded-0 text-decoration-none'>Sign Up</Link>
                     <div className='mt-2 text-center'>
                         <Link to="/forgot-password" className='custom-link'>Forgot Password?</Link>
-                        <Link to="/guest" className='custom-link'>Sign in as Guest</Link>
+                        <Link to="/guest" className='custom-link mt-2'>Sign in as Guest</Link>
                     </div>
                 </form>
             </div>
