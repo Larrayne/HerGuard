8c50fc1ee5a9e086962cad37c3b1b04d701165d6
tncube023a <tncube023a@student.wethinkcode.co.za>
2024-07-31 13:30:51 +0200

    created new branch to fix issues

---
+++ Frontend/src/Login.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './App.css'; 
+
+function Login() {
+    return (
+        <div className='d-flex justify-content-center align-items-center custom-bg vh-100'>
+            <div className='bg-pink p-3 rounded w-25'>
+                <h2>Login</h2>
+                <form action="">
+                    <div className='mb-3'>
+                        <label htmlFor='email'><strong>Email</strong></label>
+                        <input type='email' placeholder='Enter Email' className='form-control rounded-0' />
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='password'><strong>Password</strong></label>
+                        <input type='password' placeholder='Enter Password' className='form-control rounded-0' />
+                    </div>
+                    <button type='submit' className='custom-button w-100 rounded-0'>Log in</button>
+                    <p></p>
+                    <Link to="/signup" className='custom-button w-100 rounded-0 text-decoration-none'>Sign Up</Link>
+                    <div className='mt-2 text-center'>
+                        <Link to="/forgot-password" className='custom-link'>Forgot Password?</Link>
+                        <Link to="/guest" className='custom-link mt-2'>Sign in as Guest</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
