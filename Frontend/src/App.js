36a4d4e4f7234ffc2b5f9bd58dbc5e333a072a29
koketso16 <christinahkmothapo@gmail.com>
2024-08-28 01:26:40 +0200

    contacts page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Login from "./Login";
 import SignUp from "./SignUp";
 import mainPage from ".Main-page";
+import ShareRide from "./ShareRide";
+import ReportIncident from "./ReportIncident";
+import Contact from "./Contact";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,7 +14,9 @@ function App() {
         <Route path="/" element={<Login />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/main-page" element={<mainPage />}>
-          {" "}
+          <Route path="/share-ride" component={ShareRide} />
+          <Route path="/report-incident" component={ReportIncident} />
+          <Route path="/contact" component={Contact} />{" "}
         </Route>
       </Routes>
     </BrowserRouter>
