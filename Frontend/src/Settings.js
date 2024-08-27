9600b14a18cc6b62df3266890c582809d5f6acea
Kerileng Soke <kerilengsoke4@gmail.com>
2024-08-27 22:32:18 +0200

    Added settings and profile page

---
+++ Frontend/src/Settings.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Settings.css"; 
+import { useNavigate } from "react-router-dom";
+import { FaArrowLeft, FaCog } from "react-icons/fa"; // Using react-icons for back and settings icons
+
+function Settings() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="settings-container">
+      <div className="settings-header">
+        <FaArrowLeft className="icon" onClick={() => navigate(-1)} />
+        <FaCog className="icon" onClick={() => alert("Settings")} />
+      </div>
+      <div className="profile-image-container">
+        <img
+          src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Ffeeling-safe&psig=AOvVaw0eeVaZAi9JYPvOBxltgxJT&ust=1724876093236000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLC1mqn-lYgDFQAAAAAdAAAAABAE"
+          alt="Profile"
+          className="profile-image"
+        />
+      </div>
+      <div className="settings-options">
+        <button className="settings-button">My Profile</button>
+        <button className="settings-button">Notifications</button>
+        <button className="settings-button">Privacy</button>
+        <button className="settings-button">Theme</button>
+        <button className="settings-button">Help</button>
+      </div>
+      <div className="faq-container">
+        <img
+          src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-png%2Ffaq-icon&psig=AOvVaw1JGcwU8Lm8GxfWX_aKqRWB&ust=1724876164318000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKD9_8r-lYgDFQAAAAAdAAAAABAE"
+          alt="FAQ"
+          className="faq-image"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Settings;
