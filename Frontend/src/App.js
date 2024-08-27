7d211305da57d5b1dc5bbb616148ad5e7f9ffc84
koketso16 <christinahkmothapo@gmail.com>
2024-08-28 01:49:48 +0200

    all pages

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import Login from "./Login";
 import SignUp from "./SignUp";
-import mainPage from ".Main-page";
-import ShareRide from "./ShareRide";
+import MainPage from "./MainPage"; // Renamed to start with capital letter
+import ShareRide from "./ShareRide"; // Removed hyphen from the file name to keep consistent with JS naming conventions
 import ReportIncident from "./ReportIncident";
-import Contact from "./Contact";
+import Contact from "./Contacts";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/main-page" element={<mainPage />}>
-          <Route path="/share-ride" component={ShareRide} />
-          <Route path="/report-incident" component={ReportIncident} />
-          <Route path="/contact" component={Contact} />{" "}
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/main-page" element={<MainPage />}>
+          <Route path="share-ride" element={<ShareRide />} />
+          <Route path="report-incident" element={<ReportIncident />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
