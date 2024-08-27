4c653c5faebc35758114387b7dfdc4046a4dbc44
tncube023a <tncube023a@student.wethinkcode.co.za>
2024-08-27 22:30:35 +0200

    completed the faq and share location

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,13 @@
+const express = require("node");
+const mysql = require('mysql');
+const cors = require('cors');
 
+const app = express;
+app.request(cors());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "signup"
+})
