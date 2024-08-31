0639fb196d6395abb96293ac0430dff2b96ec374
JChisu@ChisuJ.(larrayne) <lrangolo023a@student.wethinkcode.co.za>
2024-08-31 15:54:01 +0200

    app.js update for home, danger alerts and community reporting

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Home from './pages/Home';
 import Login from './Login';
 import SignUp from './SignUp';
 import MainPage from './MainPage';
 import Contacts from './Contacts';
 import ReportIncidents from './ReportIncident';
+import DangerAlerts from './components/DangerAlerts/DangerAlerts';
 import FAQ from './FAQ';
 import ShareLocation from './ShareLocation';  
 import Support from './Support'
 import ForgotPassword from './ForgotPassword';
+import CommunityReporting from './components/CommunityReporting/CommunityReporting';
 import Profile from './Profile';
 import Settings from './Settings';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -19,12 +22,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />{" "}
         {/* Temporarily set a path you want to test as the default route. I have put Login for simplicity purposes but it can be changed*/}
+        <Route path='/homepage' element={<Home/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="main-page" element={<MainPage />} />
         <Route path="main-page/report-incident" element={<ReportIncidents />} />
         <Route path="main-page/share-ride" element={<ShareRide />} />
+        <Route path='/DangerAlerts' element={<DangerAlerts/>} />
         <Route path="main-page/contacts" element={<Contacts />} />
+        <Route path='/CommunityReporting' element={<CommunityReporting/>} />
         <Route path="/ShareLocation" element={<ShareLocation />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/profile" element={<Profile />} />
