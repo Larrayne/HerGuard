fe18201a149d85bd358b0252e48b1b29269f3859
JChisu@ChisuJ.(larrayne) <lrangolo023a@student.wethinkcode.co.za>
2024-08-31 15:43:48 +0200

    app.js conflict resolution

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,10 +6,10 @@ import Contacts from './Contacts';
 import ReportIncidents from './ReportIncident';
 import FAQ from './FAQ';
 import ShareLocation from './ShareLocation';  
-import Home from './pages/Home';
-import DangerAlerts from './components/DangerAlerts/DangerAlerts';
-import CommunityReporting from './components/CommunityReporting/CommunityReporting';
 import Support from './Support'
+import ForgotPassword from './ForgotPassword';
+import Profile from './Profile';
+import Settings from './Settings';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
@@ -17,19 +17,20 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />  {/* Temporarily set a path you want to test as the default route. I have put Login for simplicity purposes but it can be changed*/}
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='main-page' element={<MainPage />} />
-        <Route path='main-page/report-incident' element={<ReportIncidents />} />
-        <Route path='main-page/share-ride' element={<ShareLocation />} />
-        <Route path='main-page/contacts' element={<Contacts />} />
-        <Route path='/ShareLocation' element={<ShareLocation/>}/>
-        <Route path='/FAQ' element={<FAQ />} />  
-        <Route path='/homepage' element={<Home/>} />
-        <Route path='/DangerAlerts' element={<DangerAlerts/>} />
-        <Route path='/CommunityReporting' element={<CommunityReporting/>} />
-        <Route path='/FAQ' element={<Support />} />
+        <Route path="/" element={<Login />} />{" "}
+        {/* Temporarily set a path you want to test as the default route. I have put Login for simplicity purposes but it can be changed*/}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="main-page" element={<MainPage />} />
+        <Route path="main-page/report-incident" element={<ReportIncidents />} />
+        <Route path="main-page/share-ride" element={<ShareRide />} />
+        <Route path="main-page/contacts" element={<Contacts />} />
+        <Route path="/ShareLocation" element={<ShareLocation />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/FAQ" element={<Support />} />
       </Routes>
     </BrowserRouter>
   );
